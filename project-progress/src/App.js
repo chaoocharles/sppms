fd58ab3852b6e0f9bc3e655078148aef0c927ac0
Chaoo Charles <charleykairu@gmail.com>
2020-02-22 19:04:47 +0300

    project setup

---
+++ project-progress/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className = "badge badge-primary">SPPMS</h2>
+    </div>
+  );
+}
+
+export default App;
