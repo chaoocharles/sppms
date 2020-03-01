5e5f0ee2c429523a0937ea002e19c29438ff9bdb
Chaoo Charles <charleykairu@gmail.com>
2020-03-01 18:03:43 +0300

    Adding project

--- project-progress/src/index.js
+++ project-progress/src/index.js
@@ -3,25 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux';
-import {reactReduxFirebase, getFirebase} from 'react-redux-firebase';
-import {reduxFirestore, getFirestore} from 'redux-firestore';
+import {ReactReduxFirebaseProvider, getFirebase} from 'react-redux-firebase';
+import {createFirestoreInstance, getFirestore, reduxFirestore} from 'redux-firestore';
 import thunk from 'redux-thunk';
+
+import firebase from 'firebase/app';
 import fbConfig from './config/fbConfig';
 
 
 
-const store = createStore(rootReducer, 
+const rrfConfig = {
+    useFirestoreForProfile: true,
+    userProfile: 'users', 
+    attachAuthIsReady: true
+}
+
+  const store = createStore(rootReducer, 
     compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+    reduxFirestore(fbConfig)
     )
     );
 
-ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
+    const rrfProps = {
+        firebase,
+        config: rrfConfig,
+        dispatch: store.dispatch,
+        createFirestoreInstance
+    }
+
+ReactDOM.render(<Provider store = {store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+    <App />
+    </ReactReduxFirebaseProvider>
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- project-progress/src/store/reducers/rootReducer.js
+++ project-progress/src/store/reducers/rootReducer.js
@@ -1,10 +1,14 @@
+import firebaseReducer from 'react-redux-firebase';
+import firestoreReducer from 'redux-firestore';
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
 import {combineReducers} from 'redux'
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    project: projectReducer
+    project: projectReducer,
+    firebase: firebaseReducer,
+    firestore: firestoreReducer
 });
 
 export default rootReducer;
