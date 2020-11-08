5ff58cfa2b88e393196ead3635c5b70aa7dba5c8
chaoocharles <charleykairu@gmail.com>
2020-11-08 16:50:09 +0300

    updated signup form

--- project-progress/src/components/auth/SignUp.jsx
+++ project-progress/src/components/auth/SignUp.jsx
@@ -41,19 +41,19 @@ class SignUp extends Component {
           <h5 className="grey-text text-darken-3">Sign Up</h5>
           <div className="input-field">
             <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" onChange={this.handleChange} />
+            <input type="text" id="firstName" onChange={this.handleChange} required/>
           </div>
           <div className="input-field">
             <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" onChange={this.handleChange} />
+            <input type="text" id="lastName" onChange={this.handleChange} required/>
           </div>
           <div className="input-field">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" onChange={this.handleChange} />
+            <input type="email" id="email" onChange={this.handleChange} required/>
           </div>
           <div className="input-field">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={this.handleChange} />
+            <input type="password" id="password" onChange={this.handleChange} required/>
           </div>
           <div className="input-field">
             <button className="btn cyan darken-2 z-depth-0">SIGN Up</button>
